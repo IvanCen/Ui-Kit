7009f02ee6d72217606cd95b3dfe1818211b6f29
Ivan Senkov <ivansenkov.web@gmail.com>
2020-04-08 12:24:32 +0300

    edit radio, checkbox-slide, checkbox-textslide, text-area, main-card, footer, card-item

--- evo__chunks/__app__footer/js/main.js
+++ evo__chunks/__app__footer/js/main.js
@@ -1,6 +1,4 @@
 const footerButton = document.querySelectorAll('.footer__button');
-const footerButtonActive = 'footer__button--active';
-const footerIconActive = 'footer__icon--active';
 
 [...footerButton].forEach((item) => {
     item.addEventListener('click', function() {
@@ -12,7 +10,3 @@ const footerIconActive = 'footer__icon--active';
         this.firstElementChild.classList.add('footer__icon--active');
     })
 });
-
-
-
-//switchActive(footerButton, footerButtonActive, footerIconActive);
