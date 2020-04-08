00243c252dfcf664ebd266631748e08c9abf6685
Ivan Senkov <ivansenkov.web@gmail.com>
2020-04-08 21:37:17 +0300

    compare overlay and modal edit checkbox-slide, head style, card-item, text-area, input, footer, button fix bug modal add foot, two button

--- evo__chunks/__app__modal/js/main.js
+++ evo__chunks/__app__modal/js/main.js
@@ -4,6 +4,6 @@ const modalButtonAccept = document.querySelector('.modal__button-accept');
 document.addEventListener('click', (event) => {
     if(event.target === overlay || event.target === modalButtonAccept) {
         modal.classList.remove('modal--open');
-        overlay.classList.remove('overlay--visible');
+        overlay.classList.remove('modal__overlay--visible');
     }
 });
