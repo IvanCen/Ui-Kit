fce523eb9a70065626f978f2dc81d4769906f32d
IvanCen <ivansenkov.web@gmail.com>
2020-09-24 01:44:58 +0300

    sprint 7
    do refactor and add get document on api

--- evo__chunks/__test-app__main-page/js/main.js
+++ evo__chunks/__test-app__main-page/js/main.js
@@ -24,6 +24,8 @@ appData = {
     bonus: '',
   },
   information: {},
+  catalog: {},
+  stores: [],
 };
 
 /**
@@ -50,11 +52,19 @@ function setLocalStorageAppData() {
 }
 
 /**
+ * Функция возвращающая данные appData из локал стораджа или пустой объект если там пусто
+ * @return {object}
+ */
+function getLocalStorageAppData() {
+  return JSON.parse(localStorage.getItem('appData')) || {};
+}
+
+/**
  * Функция создания объекта для данных о каталоге и записи туда информации
  * Завершается вызовом функции записи всего объекта appData в локал сторадж
- * @param data входящие данные для установки в глобальный объект даты
+ * @param data входящие данные для установки в глобальный объект appData
  * */
-function setDataCatalog(data) {
+function updateDataCatalog(data) {
   if (!appData.catalog) {
     appData.catalog = {};
   }
@@ -67,11 +77,11 @@ function setDataCatalog(data) {
 /**
  * Функция создания объекта для данных о магазинах и записи туда информации
  * Завершается вызовом функции записи всего объекта appData в локал сторадж
- * @param data входящие данные для установки в глобальный объект даты
+ * @param data входящие данные для установки в глобальный объект appData
  * */
-function setDataStores(data) {
+function updateDataStores(data) {
   if (!appData.stores) {
-    appData.stores = {};
+    appData.stores = [];
   }
   if (data) {
     appData.stores = data.successData;
@@ -82,9 +92,9 @@ function setDataStores(data) {
 /**
  * Функция создания объекта для данных о промо-информации и записи туда информации
  * Завершается вызовом функции записи всего объекта appData в локал сторадж
- * @param data входящие данные для установки в глобальный объект даты
+ * @param data входящие данные для установки в глобальный объект appData
  * */
-function setDataPromos(data) {
+function updateDataPromos(data) {
   if (!appData.information) {
     appData.information = {};
   }
@@ -100,9 +110,9 @@ function setDataPromos(data) {
 /**
  * Функция создания объекта для данных о постах и записи туда информации
  * Завершается вызовом функции записи всего объекта appData в локал сторадж
- * @param data входящие данные для установки в глобальный объект даты
+ * @param data входящие данные для установки в глобальный объект appData
  * */
-function setDataPosts(data) {
+function updateDataPosts(data) {
   if (!appData.information) {
     appData.information = {};
   }
@@ -115,25 +125,115 @@ function setDataPosts(data) {
     }
   }
 }
-/*async function doConnectFunction() {
-  console.log('connection');
-  let status;
-  const img = document.createElement('img');
-  img.src = 'https://js.cx/clipart/train.gif';
-  img.onload = await function () {
-    return Promise.resolve(true);
-  };
-  img.onerror = await function () {
-    return false;
-  };
-  return status;
+/**
+ * Функция создания объекта для данных о публичных документах и записи туда информации
+ * Завершается вызовом функции записи всего объекта appData в локал сторадж
+ * @param data входящие данные для установки в глобальный объект appData
+ * */
+function updateDataPublicDocuments(data) {
+  if (!appData.publicDocuments) {
+    appData.publicDocuments = {};
+  }
+  if (data) {
+    appData.publicDocuments = data;
+    setLocalStorageAppData();
+  }
 }
-console.log(doConnectFunction().then((res) => console.log(res)));*/
+
+/**
+ * Функция проверки наличия публичных документов в глобальном объекте appData
+ * @return {boolean}
+ */
+function checkAvailabilityDataPublicDocuments() {
+  if (!appData.publicDocuments) {
+    return false;
+  }
+  return true;
+}
+
+async function getPublicDocument(mode, documentName) {
+  const document = await makeApiRequest('get-public-document', {
+    document: documentName,
+    mode,
+  });
+  if (document) {
+    document.successData.documentName = documentName;
+    document.successData.lastEditDateRequest = Date.now();
+  }
+  return document.successData;
+}
+
+/**
+ * Функция проверки последнего изменения документа и получение и обновление документов
+ * если требуется
+ */
+function checkLastEditDateTimeDocumentsAndGetItIfIsNeed() {
+  const { publicDocuments } = appData;
+  const newPublicDocuments = {};
+  Object.entries(publicDocuments).forEach(async ([key, value]) => {
+    // сравнение времени сейчас с последним запросом на документ,
+    // если оно больше чем день вернет true
+    if ((Date.now() - value.lastEditDateRequest) > (24 * 60 * 60 * 1000)) {
+      // запрос последнего редактирования документа
+      const publicDocumentEditDate = await getPublicDocument('editDateTime', key);
+      // если время изменилось, делаем запрос на новый документ
+      if (publicDocumentEditDate.editDate !== value.editDate) {
+        const publicDocument = await getPublicDocument('both', key);
+        if (publicDocument) {
+          // и записываем его в объект с новыми документами
+          newPublicDocuments[publicDocument.documentName] = publicDocument;
+        }
+      }
+    }
+  });
+  // присваивание с остаточными параметрами, сначала старый объект publicDocuments попадает
+  // в appData.publicDocuments потом если newPublicDocuments содержит одинаковое название ключа,
+  // он его перезапишет на новое
+  return { ...publicDocuments, ...newPublicDocuments };
+}
+
+/**
+ * Функция получения всех публичных документов
+ * @return {object}
+ */
+async function getDataPublicDocuments() {
+  const isPublicDocumentsAvailability = checkAvailabilityDataPublicDocuments();
+  const modeBoth = 'both';
+
+  if (!isPublicDocumentsAvailability) {
+    const publicDocumentsPrivacyPolicy = await getPublicDocument(modeBoth, 'privacy-policy');
+    const publicDocumentsUserAgreement = await getPublicDocument(modeBoth, 'user-agreement');
+    const publicDocumentsPublicOffer = await getPublicDocument(modeBoth, 'public-offer');
+    const publicDocumentsOurHistory = await getPublicDocument(modeBoth, 'our-history');
+
+    const publicDocuments = {};
+    // создание объекта документов и запись в него полученных данных каждого документа
+    [publicDocumentsPrivacyPolicy,
+      publicDocumentsUserAgreement,
+      publicDocumentsPublicOffer,
+      publicDocumentsOurHistory].forEach((document) => {
+      if (document) {
+        publicDocuments[document.documentName] = document;
+      }
+    });
+    return publicDocuments;
+  }
+  return checkLastEditDateTimeDocumentsAndGetItIfIsNeed();
+}
+
+/**
+ * Функция проверяющая интернет соединение
+ * @return {boolean}
+ * */
+function checkInternetConnection() {
+  return window.navigator.onLine;
+}
+
 /**
  * Функция запроса всех данных по апи для старта приложения
  * @return {Promise<void>}
  * */
-async function getAllDataApiForStartApp() {
+async function updateAllDataForStartApp() {
   const catalog = await makeApiRequest('get-catalog', {
     view: 'both',
   });
@@ -146,17 +246,26 @@ async function getAllDataApiForStartApp() {
     offset: 0,
     length: 2,
   });
+  const publicDocuments = await getDataPublicDocuments();
 
-  [catalog, stores, promos, posts].forEach((item) => {
-    if (!item) {
+  updateDataCatalog(catalog);
+  updateDataStores(stores);
+  updateDataPromos(promos);
+  updateDataPosts(posts);
+  updateDataPublicDocuments(publicDocuments);
+}
 
-    }
-  });
 
-  setDataCatalog(catalog);
-  setDataStores(stores);
-  setDataPromos(promos);
-  setDataPosts(posts);
+/**
+ * Функция инициализации данных для запуска приложения
+ * @return {Promise<void>}
+ */
+async function dataInit() {
+  const haveInternet = checkInternetConnection();
+  appData = getLocalStorageAppData(); // берет данные из локал стораджа и обновляет в глобальной переменной
+  if (haveInternet) {
+    await updateAllDataForStartApp(); // если есть интернет, то обновляет данные на актуальные
+  }
 }
 
 /**
@@ -165,7 +274,8 @@ async function getAllDataApiForStartApp() {
  * */
 async function coreInit() {
   runPreloader();
-  await getAllDataApiForStartApp();
+  await dataInit();
+  // renderMainPage();
   stopPreloader();
 }
 
